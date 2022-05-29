797e7fc5a5ae98ae48022ff1d40dc5b649dd6231
user-nexus <henrique.p.lima0@gmail.com>
2022-05-28 23:27:59 -0300

    Delete copied files

--- src/public/assets/js/local.js
+++ src/public/assets/js/local.js
@@ -1,32 +1,53 @@
-const arrayLocations = ['bh_bares', 'bh_cinemas', 'bh_museus', 'bh_parquespublicos', 'bh_restaurantes', 'bh_turisticos'];
-//intial, modify last
 const urlQuery = window.location.search;
 const urlParams = new URLSearchParams(urlQuery);
-const lc = urlParams.get('lc');
-const nb = urlParams.get('nb');
+const ct = urlParams.get('ct');
+const id = urlParams.get('id');
 
+const specificCityURL = `http://localhost:3000/${ct}/${id}`;
+const placeInfo = document.querySelector('#place');
+const placeImgs = document.querySelector('#place_imgs');
 
-let i = lc || 0;
-let j = nb || 3;
-const localBaseURL = "http://localhost:3000";
-const localURLTotal = localBaseURL + "/" + arrayLocations[j]; 
-
-const localName = document.getElementById("local-name");
-const localAdress = document.getElementById('local-adress');
-const localDescription = document.getElementById('local-description');
-const localLink = document.getElementById('local-link');
-
-fetch(localURLTotal)
+fetch(specificCityURL)
   .then(res => res.json())
-  .then(local => {
-    localName.innerHTML = `<h2>${local[i].name}</h2>`;
-    localAdress.innerHTML = `<p>${local[i].address}</p>`;
-    localDescription.innerHTML = `<p>${local[i].description}</p>`;
-    localLink.innerHTML = `<a href="${local[i].link}">${local[i].link}</a>`;
-    /* console.log("Passei aqui"); */
-  });
+  .then(place => {
+    let place_list = '';
+    let place_img_list = '';
+    
+    place_list += `
+    <div class="card my-5 py-3" style="border-radius: 20px">
+      <div class="card-body">
+        <h1>${place.name}</h1>
+      </div>
+    </div>
+    <div class="card my-5 py-3" style="border-radius: 20px">
+      <div class="card-body">
+        <h2>Endereço</h2>
+        <p>${place.address}</p>
+      </div>
+    </div>
+    <div class="card my-5 py-3" style="border-radius: 20px">
+      <div class="card-body">
+        <h2>Mapa</h2>
+      </div>
+    </div>
+    <div class="card my-5 py-3" style="border-radius: 20px">
+      <div class="card-body">
+        <h2>Link</h2>
+        <a href="${place.link}" target="_blank"><p>${place.link}</p></a>
+      </div>
+    </div>
+    `;
+    placeInfo.innerHTML = place_list;
 
-
-
-
-//Pensar na utilização do id, para cidade, mesma lógica, só mais uma query.
+    for (let i = 0; i < place.imgs.length; i++) {
+      place_img_list += `
+      <img
+        src="${place.imgs[i]}"
+        class="img-fluid p-4"
+        style="border-radius: 40px"
+        alt="..."
+      />
+      `;
+      placeImgs.innerHTML = place_img_list;
+    }
+  })
