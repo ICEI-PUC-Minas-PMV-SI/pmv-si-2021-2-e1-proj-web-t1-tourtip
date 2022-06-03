d5fe60e3c5e33a01f0a54bbe31d7713d8c350563
user-nexus <henrique.p.lima0@gmail.com>
2022-06-02 21:34:51 -0300

    Update json-server and Procfile

--- src/public/assets/js/server.js
+++ src/public/assets/js/server.js
@@ -1,3 +1,14 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(require('./db.js')());
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
 // create an express app
 const express = require("express")
 const path = require('path')
@@ -14,3 +25,4 @@ app.get("/", function (req, res) {
 // start the server listening for requests
 app.listen(process.env.PORT || 5000, 
 	() => console.log("Server is running..."));
+  
