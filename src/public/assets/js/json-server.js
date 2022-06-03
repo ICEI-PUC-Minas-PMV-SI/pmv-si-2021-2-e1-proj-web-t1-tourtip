d86d757beb87f7f0bc7b2e3d8a016aa91cb5ba49
user-nexus <henrique.p.lima0@gmail.com>
2022-06-02 21:52:59 -0300

    Test upload without express

--- src/public/assets/js/json-server.js
+++ src/public/assets/js/json-server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(require('./db.js')());
+const router = jsonServer.router(require('./index.js')());
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
